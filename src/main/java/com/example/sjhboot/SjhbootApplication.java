3cee7a94b8a8e44f515426716d0bbb0dad374b24
sjh <mapelamll@naver.com>
2023-06-11 17:04:44 +0900

    1st

---
+++ src/main/java/com/example/sjhboot/SjhbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.sjhboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjhbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjhbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sjhboot/SjhbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sjhboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjhbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
