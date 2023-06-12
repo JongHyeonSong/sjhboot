b55436d808cf8edf46556c175d42218919bf20b3
sjh <mapelamll@naver.com>
2023-06-12 23:24:54 +0900

    전역 cors Filter 사용해보기

---
+++ src/main/java/com/example/sjhboot/CorsFilter.java
@@ -0,0 +1,27 @@
+package com.example.sjhboot;
+
+import java.io.IOException;
+import java.net.http.HttpResponse;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorsFilter implements Filter {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("CHCHCH");
+
+		HttpServletResponse rr = (HttpServletResponse) response;
+		rr.setHeader("hoho", "BCBC");
+		chain.doFilter(request, response);
+	}
+}
