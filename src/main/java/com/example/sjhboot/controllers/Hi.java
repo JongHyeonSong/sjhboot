86df63c864e5a391a915cc47ab5a555e9e13c65d
sjh <mapelamll@naver.com>
2023-06-12 21:22:29 +0900

    롬복 + Logger추가

--- src/main/java/com/example/sjhboot/controllers/Hi.java
+++ src/main/java/com/example/sjhboot/controllers/Hi.java
@@ -1,13 +1,24 @@
 package com.example.sjhboot.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class Hi {
 
   @GetMapping(value = "/h1")
+
   public String getMethodName() {
+
+    // ERROR > WARN > INFO > DEBUG 순인듯?
+    log.debug("debugg");
+    log.warn("warng");
+    log.info("info");
+
     return new String("GOGO");
   }
 
